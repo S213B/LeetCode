94de07c4d7bf4abc063700ee632385fd99b48b0e
lqxu <liqunxu@fortinet.com>
2019-12-27 21:49:28 +0000

    add 105

--- 0105/Solution.cpp
+++ 0105/Solution.cpp
@@ -3,26 +3,106 @@
 #include <vector>
 #include <unordered_map>
 #include <unordered_set>
+#include <queue>
+#include <cstring>
 
 using namespace std;
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
 class Solution {
 public:
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        if (preorder.size() == 0)
+            return nullptr;
 
+        int mid = inorder[0], idx = 0;
+        while (preorder[idx] != mid) idx ++;
+
+        vector<int> pre_left(preorder.begin(), preorder.begin() + idx);
+        vector<int> pre_right(preorder.begin() + idx + 1, preorder.end());
+
+        vector<int> in_left(inorder.begin() + 1, inorder.begin() + 1 + idx);
+        vector<int> in_right(inorder.begin() + 1 + idx, inorder.end());
+
+        TreeNode *n = new TreeNode(mid);
+        n->left = buildTree(pre_left, in_left);
+        n->right = buildTree(pre_right, in_right);
+
+        return n;
     }
 };
 
+static void
+go_preorder(TreeNode *n, vector<int> &v) {
+    if (!n)
+        return;
+    go_preorder(n->left, v);
+    v.push_back(n->val);
+    go_preorder(n->right, v);
+}
+
+static void
+go_inorder(TreeNode *n, vector<int> &v) {
+    if (!n)
+        return;
+    v.push_back(n->val);
+    go_inorder(n->left, v);
+    go_inorder(n->right, v);
+}
+
+static bool
+compare_tree(TreeNode *n1, TreeNode *n2) {
+    if (!n1 && !n2)
+        return true;
+    if (!n1 || !n2)
+        return false;
+    if (n1->val != n2->val)
+        return false;
+    return compare_tree(n1->left, n2->left) &&
+           compare_tree(n1->right, n2->right);
+}
+
 int main(int argc, char *argv[]) {
     Solution solution;
+    vector<int> preorder, inorder;
+    queue<TreeNode *> q;
+    TreeNode *root = new TreeNode(atoi(argv[1]));
+    TreeNode *ans;
+
+    q.push(root);
+    for (int i = 2; i < argc; i ++) {
+        TreeNode *father = q.front();
+        q.pop();
+
+        if (strcmp(argv[i], "null")) {
+            TreeNode *child = new TreeNode(atoi(argv[i]));
+            q.push(child);
+            father->left = child;
+        }
+
+        if (++i >= argc)
+            break;
+
+
+        if (strcmp(argv[i], "null")) {
+            TreeNode *child = new TreeNode(atoi(argv[i]));
+            q.push(child);
+            father->right = child;
+        }
+    }
+
+    go_preorder(root, preorder);
+    go_inorder(root, inorder);
+
+    ans = solution.buildTree(preorder, inorder);
+
+    cout << compare_tree(root, ans) << endl;
+
     return 0;
 }
