eaccd08f4be63e205cac5ad8a5b72480577d058a
S213B <xlq.s213b@gmail.com>
2019-12-27 10:19:27 -0800

    add description of 134 and 105.

---
+++ 0105/Solution.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+
+    }
+};
+
+int main(int argc, char *argv[]) {
+    Solution solution;
+    return 0;
+}
---
+++ 0134/Solution.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+
+    }
+};
+
+int main(int argc, char *argv[]) {
+    Solution solution;
+    return 0;
+}
