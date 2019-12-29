3f8099100d1b5290516ae999f0f24842aafa2270
S213B <xlq.s213b@gmail.com>
2019-12-29 00:05:14 -0800

    update 1143

--- 1143/Solution.cpp
+++ 1143/Solution.cpp
@@ -9,25 +9,27 @@ using namespace std;
 class Solution {
 public:
     int longestCommonSubsequence(string text1, string text2) {
-        int dp[1001][1001] = {0};
+        vector<int> dp(text2.size(), 0);
 
-        dp[0][0] = (text1[0] == text2[0]) ? 1 : 0;
+        dp[0] = (text1[0] == text2[0]) ? 1 : 0;
         for (int i = 1; i < text2.size(); i ++)
-            dp[0][i] = (text1[0] == text2[i]) ? 1 : dp[0][i-1];
-        for (int i = 1; i < text1.size(); i ++)
-            dp[i][0] = (text1[i] == text2[0]) ? 1 : dp[i-1][0];
+            dp[i] = (text1[0] == text2[i]) ? 1 : dp[i-1];
 
         for (int i = 1; i < text1.size(); i ++) {
+            int prev = dp[0];
+            dp[0] = (text1[i] == text2[0]) ? 1 : dp[0];
             for (int j = 1; j < text2.size(); j ++) {
+                int _prev = dp[j];
                 if (text1[i] == text2[j]) {
-                    dp[i][j] = dp[i-1][j-1] + 1;
+                    dp[j] = prev + 1;
                 } else {
-                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                    dp[j] = max(dp[j], dp[j-1]);
                 }
+                prev = _prev;
             }
         }
 
-        return dp[text1.size()-1][text2.size()-1];
+        return dp[text2.size()-1];
     }
 };
 
