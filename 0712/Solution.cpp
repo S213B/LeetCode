5689bd5b081e8ce6d8d7014efcfb5dbe6a0f8527
S213B <xlq.s213b@gmail.com>
2019-12-28 23:52:38 -0800

    update 712

--- 0712/Solution.cpp
+++ 0712/Solution.cpp
@@ -9,31 +9,32 @@ using namespace std;
 class Solution {
 public:
     int minimumDeleteSum(string s1, string s2) {
-        int dp[1001][1001] = {0};
-        
-        dp[0][0] = (s1[0] == s2[0]) ? 0 : (s1[0] + s2[0]);
+        vector<int> dp(s2.size(), 0);
+
+        dp[0] = (s1[0] == s2[0]) ? 0 : (s1[0] + s2[0]);
         for (int i = 1, acc = s2[0]; i < s2.size(); i ++) {
-            dp[0][i] = (s1[0] == s2[i]) ? acc : dp[0][i-1] + s2[i];
+            dp[i] = (s1[0] == s2[i]) ? acc : dp[i-1] + s2[i];
             acc += s2[i];
         }
-        for (int i = 1, acc = s1[0]; i < s1.size(); i ++) {
-            dp[i][0] = (s1[i] == s2[0]) ? acc : dp[i-1][0] + s1[i];
-            acc += s1[i];
-        }
 
-        for (int i = 1; i < s1.size(); i ++) {
+        for (int i = 1, acc = s1[0]; i < s1.size(); i ++) {
+            int prev = dp[0];
+            dp[0] = (s1[i] == s2[0]) ? acc : dp[0] + s1[i];
+            acc += s1[i];
             for (int j = 1; j < s2.size(); j ++) {
+                int _prev = dp[j];
                 if (s1[i] == s2[j]) {
-                    dp[i][j] = dp[i-1][j-1];
+                    dp[j] = prev;
                 } else {
-                    dp[i][j] = dp[i-1][j-1] + s1[i] + s2[j];
-                    dp[i][j] = min(dp[i][j], dp[i-1][j] + s1[i]);
-                    dp[i][j] = min(dp[i][j], dp[i][j-1] + s2[j]);
+                    dp[j] += s1[i];
+                    dp[j] = min(dp[j], prev + s1[i] + s2[j]);
+                    dp[j] = min(dp[j], dp[j-1] + s2[j]);
                 }
+                prev = _prev;
             }
         }
 
-        return dp[s1.size()-1][s2.size()-1];
+        return dp[s2.size()-1];
     }
 };
 
