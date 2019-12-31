db489baefd8411d60bceda4dc196adc85a379d2e
S213B <xlq.s213b@gmail.com>
2019-12-30 23:42:41 -0800

    add 875

--- 0875/Solution.cpp
+++ 0875/Solution.cpp
@@ -7,13 +7,44 @@
 using namespace std;
 
 class Solution {
+private:
+    int eat_all(vector<int> &piles, int K) {
+        int h = 0;
+        for (auto p : piles)
+            h += (p + K - 1) / K;
+        return h;
+    }
+
 public:
     int minEatingSpeed(vector<int>& piles, int H) {
+        int l = 1, h = -1;
 
+        for (auto p : piles)
+            h = max(h, p);
+
+        while (l <= h) {
+            int m = l + (h - l) / 2;
+            int t = eat_all(piles, m);
+            if (t > H) {
+                l = m + 1;
+            } else {
+                h = m - 1;
+            }
+        }
+
+        return l;
     }
 };
 
 int main(int argc, char *argv[]) {
     Solution solution;
+    int H = atoi(argv[1]);
+    vector<int> piles;
+
+    for (int i = 2; i < argc; i ++)
+        piles.push_back(atoi(argv[i]));
+
+    cout << solution.minEatingSpeed(piles, H) << endl;
+
     return 0;
 }
