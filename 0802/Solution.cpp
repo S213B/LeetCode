65a15a4bb6047537c4bb70f7fb01960bee94c05f
lqxu <liqunxu@fortinet.com>
2020-01-16 20:54:37 +0000

    add 802

--- 0802/Solution.cpp
+++ 0802/Solution.cpp
@@ -11,13 +11,64 @@
 using namespace std;
 
 class Solution {
+private:
+    bool dfs(int cur, vector<vector<int>> &graph, vector<bool> &visited, vector<int> safe) {
+        if (!graph[cur].size())
+            return true;
+        if (safe[cur] != -1)
+            return safe[cur] != 0;
+        if (visited[cur])
+            return false;
+        visited[cur] = true;
+        for (int i = 0; i < graph[cur].size(); i ++) {
+            int next = graph[cur][i];
+            if (safe[next] == -1)
+                safe[next] = dfs(next, graph, visited, safe);
+            if (!safe[next]) {
+                safe[cur] = 0;
+                return false;
+            }
+        }
+        safe[cur] = 1;
+        return true;
+    }
+
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        int n = graph.size();
+        vector<int> ans;
+        vector<int> safe(n, -1);
 
+        for (int i = 0; i < n; i ++) {
+            vector<bool> visited(n);
+            if (dfs(i, graph, visited, safe))
+                ans.push_back(i);
+        }
+
+        return ans;
     }
 };
 
 int main(int argc, char *argv[]) {
     Solution solution;
+    vector<vector<int>> graph;
+    vector<int> row, ans;
+    int n;
+
+    while (cin >> n) {
+        if (n == -1) {
+            graph.push_back(row);
+            row.clear();
+        } else {
+            row.push_back(n);
+        }
+    }
+
+    ans = solution.eventualSafeNodes(graph);
+
+    for (auto an : ans)
+        cout << an << " ";
+    cout << endl;
+
     return 0;
 }
