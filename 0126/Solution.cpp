17385a3f1213fdb29a8fe8f052209ade7a7a34f1
S213B <xlq.s213b@gmail.com>
2020-02-07 00:30:16 -0800

    update 126

--- 0126/Solution.cpp
+++ 0126/Solution.cpp
@@ -9,177 +9,93 @@
 
 using namespace std;
 
-#if 1
-class Solution {
-public:
-    vector<vector<string>> findLadders(string beginWord, string endWord, vector<string>& wordList) {
-        unordered_set<string> word(wordList.begin(), wordList.end());
-        unordered_map<string, vector<string>> path;
-        vector<vector<string>> rc;
-        unordered_set<string> s_top, s_bot, next_layer;
-        bool is_end = false;
-
-        if (!word.count(endWord))
-            return rc;
-
-        word.erase(beginWord);
-        word.erase(endWord);
-
-        s_top.insert(beginWord);
-        s_bot.insert(endWord);
-
-        while (!s_top.empty() && !s_bot.empty()) {
-            unordered_set<string> *s1, *s2;
-            bool is_top2bot = true;
-
-            if (s_top.size() < s_bot.size()) {
-                s1 = &s_top;
-                s2 = &s_bot;
-                is_top2bot = true;
-            } else {
-                s1 = &s_bot;
-                s2 = &s_top;
-                is_top2bot = false;
-            }
-
-            for (auto s = s1->begin(); s != s1->end(); s++) {
-                string str = *s;
-                for (char &c : str) {
-                    const char orig = c;
-                    for (c = 'a'; c < 'z'; c++) {
-                        if (s2->count(str)) {
-                            if (is_top2bot)
-                                path[*s].push_back(str);
-                            else
-                                path[str].push_back(*s);
-                            is_end = true;
-                        } else if (!is_end && word.count(str)) {
-                            if (is_top2bot)
-                                path[*s].push_back(str);
-                            else
-                                path[str].push_back(*s);
-                            next_layer.insert(str);
-                        }
-                    }
-                    c = orig;
-                }
-            }
-
-            if (is_end) break;
-
-            for (const auto &s : next_layer) {
-                word.erase(s);
-            }
-
-            s1->swap(next_layer);
-            next_layer.clear();
-        }
-
-        if (is_end) {
-            vector<string> t = {beginWord};
-            gen_res(rc, path, path[beginWord], t, endWord);
-        }
-
-        return rc;
-    }
-
-    void gen_res(vector<vector<string>> &rc, unordered_map<string, vector<string>> &path, vector<string> &next, vector<string> p, string &end) {
-        for (const auto &n : next) {
-            p.push_back(n);
-            if (n == end) {
-                rc.push_back(p);
-            } else if (path.count(n)) {
-                gen_res(rc, path, path[n], p, end);
-            }
-            p.pop_back();
-        }
-    }
-};
-#else
 class Solution {
 private:
-    bool can_transform(string &a, string &b) {
+    bool can_go(string &a, string &b) {
         int diff = 0;
-        for (int i = 0; i < a.size(); i ++) {
-            if (a[i] != b[i])
-                diff ++;
-        }
+        for (int i = 0; i < a.size(); i ++)
+            diff += (a[i] == b[i] ? 0 : 1);
         return diff == 1;
     }
 
-    void dfs(int idx, vector<vector<int>> &path, vector<string> &wordList, deque<string> &an, vector<vector<string>> &ans, string &beginWord) {
-        if (idx == -1) {
-            an.push_front(beginWord);
-            ans.emplace_back(an.cbegin(), an.cend());
-            an.pop_front();
+    void dfs(vector<string> &wordList, vector<vector<int>> &go, int cur, int end_idx, vector<vector<string>> &ans, vector<string> &an) {
+        an.push_back(wordList[cur]);
+        if (cur == end_idx) {
+            ans.push_back(an);
+            an.pop_back();
             return;
         }
-        an.push_front(wordList[idx]);
-        for (int i = 0; i < path[idx].size(); i ++)
-            dfs(path[idx][i], path, wordList, an, ans, beginWord);
-        an.pop_front();
+        for (int i = 0; i < go[cur].size(); i ++)
+            dfs(wordList, go, go[cur][i], end_idx, ans, an);
+        an.pop_back();
     }
-    
+
 public:
     vector<vector<string>> findLadders(string beginWord, string endWord, vector<string>& wordList) {
-        vector<vector<string>> ans;
-        int word_sz = wordList.size();
-        int end_idx = -1;
-        vector<vector<bool>> can_go(word_sz, vector<bool>(word_sz));
         unordered_set<int> not_visited;
-        vector<vector<int>> path(word_sz, vector<int>());
-        queue<int> q;
+        unordered_set<int> top_visited, down_visited, *self, *peer;
+        vector<vector<int>> go(wordList.size()+1, vector<int>());
+        bool done = false;
+        vector<vector<string>> ans;
 
-        for (int i = 0; i < word_sz; i ++) {
-            if (!endWord.compare(wordList[i]))
+        int end_idx = -1, start_idx = -1;
+        for (int i = 0; i < wordList.size(); i ++) {
+            if (wordList[i] == endWord)
                 end_idx = i;
-            for (int j = i + 1; j < word_sz; j ++) {
-                if (can_transform(wordList[i], wordList[j])) {
-                    can_go[i][j] = can_go[j][i] = true;
-                }
-            }
             not_visited.insert(i);
         }
-        if (end_idx == -1)
+        if (end_idx < 0)
             return ans;
 
-        for (int i = 0; i < word_sz; i ++) {
-            if (can_transform(beginWord, wordList[i])) {
-                q.push(i);
-                not_visited.erase(i);
-                path[i].push_back(-1);
-            }
+        if (start_idx < 0) {
+            start_idx = wordList.size();
+            wordList.push_back(beginWord);
         }
+        top_visited.insert(start_idx);
+        down_visited.insert(end_idx);
 
-        while (q.size() && !path[end_idx].size()) {
-            vector<int> just_visited;
-            int q_sz = q.size();
-            for (int i = 0; i < q_sz; i ++) {
-                int s = q.front();
-                q.pop();
-                for (auto j : not_visited) {
-                    if (can_go[s][j]) {
-                        if (!path[j].size())
-                            q.push(j);
-                        path[j].push_back(s);
-                        just_visited.push_back(j);
+        while (top_visited.size() && down_visited.size() && !done) {
+            bool top2down;
+            if (top_visited.size() <= down_visited.size()) {
+                self = &top_visited;
+                peer = &down_visited;
+                top2down = true;
+            } else {
+                self = &down_visited;
+                peer = &top_visited;
+                top2down = false;
+            }
+
+            unordered_set<int> visited_now;
+            for (auto it = self->begin(); it != self->end(); it ++) {
+                int n = *it;
+                not_visited.erase(n);
+            }
+            for (auto from = self->begin(); from != self->end(); from ++) {
+                int cur = *from;
+                for (auto to = not_visited.begin(); to != not_visited.end(); to ++) {
+                    int next = *to;
+                    if (can_go(wordList[cur], wordList[next])) {
+                        if (top2down)
+                            go[cur].push_back(next);
+                        else
+                            go[next].push_back(cur);
+                        if (peer->count(next))
+                            done = true;
+                        visited_now.insert(next);
                     }
                 }
             }
-            for (auto v : just_visited)
-                not_visited.erase(v);
+
+            self->swap(visited_now);
         }
 
-        for (int i = 0; i < path[end_idx].size(); i ++) {
-            deque<string> an{endWord};
-            int j = path[end_idx][i];
-            dfs(j, path, wordList, an, ans, beginWord);
-        }
+        vector<string> an;
+        dfs(wordList, go, start_idx, end_idx, ans, an);
 
         return ans;
     }
 };
-#endif
 
 int main(int argc, char *argv[]) {
     Solution solution;
@@ -196,7 +112,7 @@ int main(int argc, char *argv[]) {
 
     for (auto &an : ans) {
         for (auto a : an)
-            cout << a << endl;
+            cout << a << " ";
         cout << endl;
     }
 
