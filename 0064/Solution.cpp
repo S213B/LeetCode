db694789fb71931905dcbc425bf1ee8558bbb241
S213B <xlq.s213b@gmail.com>
2020-02-09 20:01:07 -0800

    update 64

--- 0064/Solution.cpp
+++ 0064/Solution.cpp
@@ -11,33 +11,22 @@
 using namespace std;
 
 class Solution {
-private:
-    int dfs(vector<vector<int>> &grid, vector<vector<int>> &dis, int i, int j, int sum, int &ans) {
-        if (i >= grid.size() || j >= grid[i].size())
-            return -1;
-        if (i == grid.size()-1 && j == grid[i].size()-1) {
-            ans = min(ans, sum + grid[i][j]);
-            return grid[i][j];
-        }
-        if (dis[i][j] < 0) {
-            int r1 = dfs(grid, dis, i+1, j, sum+grid[i][j], ans);
-            int r2 = dfs(grid, dis, i, j+1, sum+grid[i][j], ans);
-            dis[i][j] = r1 < 0 ? r2 : r2 < 0 ? r1 : min(r1, r2);
-            dis[i][j] += grid[i][j];
-        }
-        sum += dis[i][j];
-        ans = min(ans, sum);
-        return dis[i][j];
-    }
-
 public:
     int minPathSum(vector<vector<int>>& grid) {
-        int ans = INT_MAX;
-        vector<vector<int>> dis(grid.size(), vector<int>(grid[0].size(), -1));
+        if (!grid.size())
+            return 0;
 
-        dfs(grid, dis, 0, 0, 0, ans);
+        vector<int> dp(grid[0].size(), INT_MAX);
 
-        return ans;
+        dp[0] = 0;
+        for (int i = 0; i < grid.size(); i ++) {
+            dp[0] += grid[i][0];
+            for (int j = 1; j < grid[i].size(); j ++) {
+                dp[j] = min(dp[j], dp[j-1]) + grid[i][j];
+            }
+        }
+
+        return dp.back();
     }
 };
 
